7c8f552918f5d63786dbfdd40f65aacbc4781c2d
ErwanTLG <erwan.tlg@protonmail.com>
2022-02-12 10:48:23 +0100

    Code cleanup

--- main.ml
+++ main.ml
@@ -1,6 +1,8 @@
 open Graphics
 open Terrain
 
+let dimensions = 32, 32
+
 let char_of_case c =
   match c with
   | Eau -> ' '
@@ -30,7 +32,7 @@ exception Fin
 
 let main =
   open_graph (Printf.sprintf " %dx%d" (16+tw*gw) (50+th*gh));
-  let t = genere_terrain (32, 32) 1234 in
+  let t = genere_terrain dimensions 1234 in
   for i = 0 to Array.length t - 1 do
     for j = 0 to Array.length t.(0) - 1 do
       putchar white black i j (char_of_case t.(i).(j))
@@ -41,70 +43,3 @@ let main =
     let s = wait_next_event [Key_pressed] in
     if s.key = 'q' then raise Fin
   done
-
-
-
-
-
-
-
-
-
-
-
-let () = print_endline "Bonjour, Monde!"
-
-type Piece  = Bombardier | Cavalier | Bouclier | Unite | General | Officier | Lancier | Archer | Ouvrier | Souverain | Garde | Chambellan
-
-type cases = Eau | Sol of Piece option | Pont | Mur | Arbre | Rocher | Camp
-
-let creer_plateau_basique =
-  let t = Array.make_matrix 34 34 None in
-  
-  for i = 0 to Array.length t-1 do
-    for j = 0 to Array.length t-1 do
-      t.(i).(j) <- Some Sol None
-    done
-  done; 
- 
-  for i = 0 to 4 do
-    for j = 0 to 4-i do
-      t.(i).(j) <- None
-    done
-  done;
-  
-  t.(9).(15) <- Some Mur;
-  t.(9).(14) <- Some Mur;
-  t.(9).(13) <- Some Mur;
-  t.(9).(11) <- Some Mur;
-  t.(10).(10) <- Some Mur;
-  t.(11).(9) <- Some Mur;
-  t.(12).(8) <- Some Mur;
-  t.(13).(8) <- Some Mur;
-  t.(14).(8) <- Some Mur;
-  t.(15).(8) <- Some Mur;
-
-  for i = 0 to (Array.length t - 1)/2 do
-    for j = 0 to (Array.length t - 1)/2 do
-      t.(Array.length t-1-i).(j) <- t.(i).(j);
-      t.(i).(Array.length t-1-j) <- t.(i).(j);
-      t(Array.length t-1-i).(Array.length t-1-j) <- t.(i).(j)
-    done
-  done;
-
-
-let deplacements_possibles plateau i j =
-  let l = [] in
-  match plateau.(i).(j) with
-  |Sol None -> l
-  |Sol Bombardier -> (i+3,j)::(i-3::j)::(i,j+3)::(i,j-3)::(i+1,j)::(i-1::j)::(i,j+1)::(i,j-1)::l 
-  (*il faut tester l'orientation de la pièce pour savoir dans quelle direction elle peut avancer de 3 et dans quelles de 1 *)
-  |Sol Unite -> (i+1,j)::(i-1::j)::(i,j+1)::(i,j-1)::l 
-  |Sol Cavalier -> for k = 1 to 4 do
-                     for m = 1 to 4 do
-                       (i-k,j)::(i-m,j)::(i+k,j)::(i+m,j)::(i-k,j-k)::(i-m,j-k)::(i+k,j-k)::(i+m,j-k)::(i-k,j+k)::(i-m,j+k)::(i+k,j+k)::(i+m,j+k)::(i-k,j-m)::(i-m,j-m)::(i+k,j-m)::(i+m,j-m)::(i-k,j+m)::(i-m,j+m)::(i+k,j+m)::(i+m,j+m)::(i,j+k)::(i,j-k)::(i,j+m)::(i,j-m)::l
-                     done
-                   done
-  |Sol Bouclier ->(i+2,j)::(i-2::j)::(i,j+2)::(i,j-2)::l
-  |Sol Lancier -> (i+2,j)::(i-2::j)::(i,j+2)::(i,j-2)::l
-  |(*flemme de faire la suite*)
--- terrain.ml
+++ terrain.ml
@@ -1,7 +1,8 @@
-type case = Eau | Terrain | Obstacle | Mur
+type cases = Eau | Sol | Pont | Mur | Arbre | Rocher | Camp
 
 let freq_eau = 5
-let freq_obstacle = 15
+let freq_arbre = 10
+let freq_rocher = 5
 
 (* génère aléatoirement un terrai de taille x * y en utilisant la graine seed *)
 let genere_terrain (x, y) seed = 
@@ -12,10 +13,29 @@ let genere_terrain (x, y) seed =
       let r = Random.int 100 in
       if r < freq_eau then
         t.(i).(j) <- Eau
-      else if r < freq_obstacle then
-        t.(i).(j) <- Obstacle
+      else if r < freq_arbre then
+        t.(i).(j) <- Arbre
+      else if r < freq_rocher then
+        t.(i).(j) <- Rocher
       else
         t.(i).(j) <- Terrain
+    done
+  done;
+  t.(9).(15) <- Some Mur;
+  t.(9).(14) <- Some Mur;
+  t.(9).(13) <- Some Mur;
+  t.(9).(11) <- Some Mur;
+  t.(10).(10) <- Some Mur;
+  t.(11).(9) <- Some Mur;
+  t.(12).(8) <- Some Mur;
+  t.(13).(8) <- Some Mur;
+  t.(14).(8) <- Some Mur;
+  t.(15).(8) <- Some Mur;
+  for i = 0 to (Array.length t - 1)/2 do
+    for j = 0 to (Array.length t - 1)/2 do
+      t.(Array.length t-1-i).(j) <- t.(i).(j);
+      t.(i).(Array.length t-1-j) <- t.(i).(j);
+      t(Array.length t-1-i).(Array.length t-1-j) <- t.(i).(j)
     done
   done;
   t
@@ -34,4 +54,3 @@ let affiche_terrain t =
     printf "\n"
   done;
   flush stdout
-
