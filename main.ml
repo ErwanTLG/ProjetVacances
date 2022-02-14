9d9d359519337a866ff3be36eca67cc95cfa1cf7
ErwanTLG <erwan.tlg@protonmail.com>
2022-02-14 23:16:11 +0100

    Ajout d'un retour visuel pour la sélection d'unité.
    RHAAAAH J'EN AI MARRE DES BUGS DE **** QUI N'ONT PAS DE SENS (ça m'a pris 1h TT, je pète un câble)

--- main.ml
+++ main.ml
@@ -41,23 +41,47 @@ let est_valide (i, j) t =
 
 exception Fin
 
-let piece_selectionnee : piece ref = ref None
+let piece_selectionnee : piece option ref = ref None
+
+let tour_attaquant = ref true (* c'est l'attaquant qui joue en premier *)
+
+(* vérifie si la piece aux coordonnées x, y appartient au joueur actif *)
+let appartient_joueur_actif x y =
+  let p = pieces.(x).(y) in
+  match p with
+  | None -> false
+  | Some pc -> pc.attaquant = !tour_attaquant
 
 let main =
+  (* TODO supprimer la ligne qui suit, juste pour le test*)
+  pieces.(0).(0) <- Some {t = Archer; x = 0; y = 0; attaquant = true};
   open_graph (Printf.sprintf " %dx%d" (16+tw*gw) (50+th*gh));
+  set_window_title "Projet Vacances";
   let t = genere_terrain (32, 32) 1234 in
   while true do
+    
+    (* dessine le terrain *)
+    set_color black;
     for i = 0 to Array.length t - 1 do
       for j = 0 to Array.length t.(0) - 1 do
         putchar white black i j (char_of_case t.(i).(j))
       done
     done;
-    (* dessine un rectangle autour de la case sous la souris *)
-    let m_x, m_y = to_grid_coords (mouse_pos ()) in
-    if est_valide (m_x, m_y) t then draw_rect (m_x * gw - 1) (m_y * gh - 1) (gw + 1) (gh + 1);
-    if button_down () && est_valide (m_x, m_y) pieces then piece_selectionnee := pieces.(m_x).(m_y)
+    
+    (* TODO ici il faudrait dessiner les pièces par-dessus le terrain *)
+
+    (* dessine un rectangle rouge autour de la case de l'unité selectionnée *)
+    (* je ne sais pas pourquoi, mais si on écrit ce match après le prochain, rien ne marche. CA M'A PRIS QUASI 1H A TROUVER CE BUG J'EN PEUX PLUS *)
+    set_color red;
+    (match !piece_selectionnee with
+    | None -> ()
+    | Some pc -> draw_rect (pc.x * gw - 1) (pc.y * gh - 1) (gw + 1) (gh + 1));
+
+    let status = wait_next_event [Button_down] in
+    let m_x, m_y = to_grid_coords(status.mouse_x, status.mouse_y) in
+
+    (* si on clique sur l'unité sélectionnée, ça la déselectionne *)
+    (match !piece_selectionnee with
+    | None -> if est_valide (m_x, m_y) pieces && appartient_joueur_actif m_x m_y then piece_selectionnee := pieces.(m_x).(m_y)
+    | Some pc -> if pc.x = m_x && pc.y = m_y then piece_selectionnee := None);
   done
-
-(* il y a actuellement de gros problèmes d'affichage, il faudrait paufiner la façon dont le rendu est fait *) 
-(* mais la logique du jeu peut néanmoins être appliquée et fonctionner *)
-
--- pieces.ml
+++ pieces.ml
@@ -1,19 +1,30 @@
-type piece  = None | Bombardier | Cavalier | Bouclier | Unite | General | Officier | Lancier | Archer | Ouvrier | Souverain | Garde | Chambellan
+type t_piece = Bombardier | Cavalier | Bouclier | Unite | General | Officier | Lancier | Archer | Ouvrier | Souverain | Garde | Chambellan
+type piece = {t: t_piece; x: int; y: int; attaquant: bool}
 
 (* ne pas oublier de reporter les dimensions du jeu ici lorsqu'on les change *)
-let pieces = Array.make_matrix 32 32 None
+let pieces : piece option array array = Array.make_matrix 32 32 None
 
 let deplacements_possibles i j =
-  match pieces.(i).(j) with
+  let aux t =
+    match t with
+    | Bombardier | Archer | Unite -> [(i+1, j); (i-1, j); (i, j+1); (i, j-1)]
+    | Cavalier ->(* for k = 1 to 4 do  TODO FIX THIS: ça ne peut pas marcher actuellement*)
+                 (*      for m = 1 to 4 do
+                         (i-k,j)::(i-m,j)::(i+k,j)::(i+m,j)::(i-k,j-k)::(i-m,j-k)::(i+k,j-k)::(i+m,j-k)::(i-k,j+k)::(i-m,j+k)::(i+k,j+k)::(i+m,j+k)::(i-k,j-m)::(i-m,j-m)::(i+k,j-m)::(i+m,j-m)::(i-k,j+m)::(i-m,j+m)::(i+k,j+m)::(i+m,j+m)::(i,j+k)::(i,j-k)::(i,j+m)::(i,j-m)::[]
+                       done
+                     done*) [] (* je renvoie temporairement une liste vide pour pas me casser la tête et que le code compile *)
+    | Bouclier -> [(i+2, j); (i-2, j); (i, j+2); (i, j-2)]
+    | Lancier -> [(i+1, j+2); (i+1, j-2); (i-1, j+2); (i-1, j-2)]
+    | General | Officier | Garde -> [(i+3, j); (i, j+3); (i-3, j); (i, j-3); (i+2, j+2); (i-2, j-2); (i-2, j+2); (i+2, j-2)]
+    | Chambellan -> [(i+3,j+3); (i-3, j-3); (i-3, j+3); (i+3, j-3)]
+    | Ouvrier | Souverain -> [(i+1, j); (i-1, j); (i, j+1); (i, j-1); (i+1, j+1); (i-1, j-1); (i-1, j+1); (i+1, j-1)]
+  in
+  let p = pieces.(i).(j) in
+  match p with
   | None -> []
-  | Bombardier | Archer | Unite -> [(i+1, j); (i-1, j); (i, j+1); (i, j-1)]
-  | Cavalier ->(* for k = 1 to 4 do  TODO FIX THIS: ça ne peut pas marcher actuellement*)
-               (*      for m = 1 to 4 do
-                        (i-k,j)::(i-m,j)::(i+k,j)::(i+m,j)::(i-k,j-k)::(i-m,j-k)::(i+k,j-k)::(i+m,j-k)::(i-k,j+k)::(i-m,j+k)::(i+k,j+k)::(i+m,j+k)::(i-k,j-m)::(i-m,j-m)::(i+k,j-m)::(i+m,j-m)::(i-k,j+m)::(i-m,j+m)::(i+k,j+m)::(i+m,j+m)::(i,j+k)::(i,j-k)::(i,j+m)::(i,j-m)::[]
-                     done
-                   done*) [] (* je renvoie temporairement une liste vide pour pas me casser la tête et que le code compile *)
-  | Bouclier -> [(i+2, j); (i-2, j); (i, j+2); (i, j-2)]
-  | Lancier -> [(i+1, j+2); (i+1, j-2); (i-1, j+2); (i-1, j-2)]
-  | General | Officier | Garde -> [(i+3, j); (i, j+3); (i-3, j); (i, j-3); (i+2, j+2); (i-2, j-2); (i-2, j+2); (i+2, j-2)]
-  | Chambellan -> [(i+3,j+3); (i-3, j-3); (i-3, j+3); (i+3, j-3)]
-  | Ouvrier | Souverain -> [(i+1, j); (i-1, j); (i, j+1); (i, j-1); (i+1, j+1); (i-1, j-1); (i-1, j+1); (i+1, j-1)]
+  | Some pc -> aux pc.t
+
+let deplace p x y =
+  pieces.(p.x).(p.y) <- None;
+  pieces.(x).(y) <- Some {t = p.t; x = x; y = y; attaquant = p.attaquant}
+
